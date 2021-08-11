c3cf0060d8900f30777c8d1c463b50dc1ae64e00
Daniel Madaghjian <danielmadaghjian@gmail.com>
2021-08-11 11:50:42 +0100

    reverted to original index.html

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -3,7 +3,7 @@
   @section('content')
       <div class="wrapper pizza-index">
 
-          <h1>This is a test. Pizza Orders</h1>     
+          <h1> Pizza Orders</h1>     
                 
                 @foreach($pizzas as $pizza)
                   <div class = "pizza-item">
